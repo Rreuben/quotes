fc2610f6f85aa77115f1c059aafdeda282e4b9a6
Rreuben <jreuben07@gmail.com>
2018-05-28 09:59:43 +0300

    Add bootstrap and webApp structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuotesDetailComponent,
+    QuotesFormComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule
